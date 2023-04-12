a9c1b8fe6d31af1c6c7fd0b6ad7d5feada9a7346
rkyadav271098 <47470666+rkyadav271098@users.noreply.github.com>
2023-04-12 08:33:12 +0530

    assertion corrected

--- src/test/java/com/test/Test.java
+++ src/test/java/com/test/Test.java
@@ -6,7 +6,7 @@ public class Test {
     
     @org.junit.jupiter.api.Test
     public void testRk(){
-        Assertions.assertEquals("10", "2");
+        Assertions.assertEquals("10", "10");
     }
     
 }
